2e91c8398c89e297a68e3c0569cd45e9ec1d6ad9
S M FIRDOUS HASSAN <153954831+firdoushassan@users.noreply.github.com>
2024-12-10 11:24:52 +0530

    added gamover screen

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -50,7 +50,7 @@ export default function game(){
         }
         k.play("hurt", {volume: 0.5});
         k.setData("current-score", score);
-        k.go("gameover", { citySfx });
+        k.go("gameover", citySfx);
     });
 
     sonic.onCollide("ring", (ring) =>{
